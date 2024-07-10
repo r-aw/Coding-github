9855bfcb3c76a5af3c465b98d087b1cafa4801ed
r-aw <156027775+r-aw@users.noreply.github.com>
2024-07-10 15:50:19 +0100

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,12 @@
+function changeNumber(n) {
+  //document.querySelector(".display").innerHTML = "";
+  result = n;
+  document.querySelector(".display").innerHTML = parseInt(n);
+}
+
+function addNumbers() {
+  let number1 = parseInt(document.querySelector(".one").value);
+  let number2 = parseFloat(document.querySelector(".two").value);
+  let = number1 + number2;
+  console.log(addNumbers());
+}
