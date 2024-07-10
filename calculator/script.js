ceeb3e0461b05078c812d5386711735b80baa3c9
Igor Rocha <igoreidji92@hotmail.com>
2024-07-10 18:16:58 +0100

    modified:   calculator/script.js

--- calculator/script.js
+++ calculator/script.js
@@ -4,9 +4,3 @@ function changeNumber(n) {
   document.querySelector(".display").innerHTML = parseInt(n);
 }
 
-function addNumbers() {
-  let number1 = parseInt(document.querySelector(".one").value);
-  let number2 = parseFloat(document.querySelector(".two").value);
-  let = number1 + number2;
-  console.log(addNumbers());
-}
